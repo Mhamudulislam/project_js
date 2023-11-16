4080a6e974ea38c8e66c4e3bd36e0c5e18b49f02
Mhamudulislam <141499999+Mhamudulislam@users.noreply.github.com>
2023-11-16 16:21:08 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,62 @@
+function forHtml()
+{document.getElementById('kldfjg').src="for_anime/stand.png";}
+
+function forHtml2()
+{document.getElementById('kldfjg').src="for_anime/goku0001.png";}
+
+function forHtml3()
+{document.getElementById('kldfjg').src="for_anime/goku0002.png";}
+
+function forHtml4()
+{document.getElementById('kldfjg').src="for_anime/goku0003.png";}
+
+function forHtm5()
+{document.getElementById('kldfjg').src="for_anime/goku0004.png";}
+
+function forHtml6()
+{document.getElementById('kldfjg').src="for_anime/goku0005.png";}
+
+function forHtml7()
+{document.getElementById('kldfjg').src="for_anime/goku0006.png";}
+
+function forHtml8()
+{document.getElementById('kldfjg').src="for_anime/goku0007.png";}
+
+function forHtml9()
+{document.getElementById('kldfjg').src="for_anime/goku0008.png";}
+
+function forHtml10()
+{document.getElementById('kldfjg').src="for_anime/goku0009.png";}
+
+function forHtml11()
+{document.getElementById('kldfjg').src="for_anime/goku0010.png";}
+
+function forHtml12()
+{document.getElementById('kldfjg').src="for_anime/goku0011.png";}
+
+function forHtml13()
+{document.getElementById('kldfjg').src="for_anime/goku0012.png";}
+
+function forHtml14()
+{document.getElementById('kldfjg').src="for_anime/goku0013.png";}
+
+function forHtml15()
+{document.getElementById('kldfjg').src="for_anime/goku0014.png";}
+
+function forHtml16()
+{document.getElementById('kldfjg').src="for_anime/goku0015.png";}
+
+function forHtml17()
+{document.getElementById('kldfjg').src="for_anime/goku0016.png";}
+
+function forHtml18()
+{document.getElementById('kldfjg').src="for_anime/goku0017.png";}
+
+function forHtml19()
+{document.getElementById('kldfjg').src="for_anime/goku0018.png";}
+
+function forHtml20()
+{document.getElementById('kldfjg').src="for_anime/goku0019.png";}
+
+function forHtml21()
+{document.getElementById('kldfjg').src="for_anime/goku0020.png";}
